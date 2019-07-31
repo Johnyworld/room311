0f96699dcaa067527f8ce2a0079801bb606fefee
Johny Kim <johnyworld@naver.com>
2019-07-31 19:44:43 +0900

    Updates

---
+++ precache-manifest.8b031f77f048ad8492d4463f9ef76929.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90e3da3dc4fbf2a49f5965a27fbd65f",
+    "url": "/room311/index.html"
+  },
+  {
+    "revision": "9319e767ad8f547e901a",
+    "url": "/room311/static/css/main.a4bf66f5.chunk.css"
+  },
+  {
+    "revision": "8c7878a04e888786c31b",
+    "url": "/room311/static/js/2.a7f65ebf.chunk.js"
+  },
+  {
+    "revision": "9319e767ad8f547e901a",
+    "url": "/room311/static/js/main.e42dcdb1.chunk.js"
+  },
+  {
+    "revision": "463b511194d795fd2e49",
+    "url": "/room311/static/js/runtime~main.f0fed031.js"
+  }
+]);
